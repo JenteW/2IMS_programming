c59e5ee4a62aa6986d8a55ebf0d60457e8d5f770
Jente Wauman <72928027+JenteW@users.noreply.github.com>
2023-09-22 11:13:42 +0200

    les1_Pauze

---
+++ programming_advanced/week1/sorting/sorting/Guessing.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting
+{
+    internal class Guessing
+    {
+        public int Linear(int[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == number)
+                { return i; }
+            }
+            return -1;
+        }
+
+        public int Stupid(int[] array, int number)
+        {
+            List<int> guessed  = new List<int>();
+            Random random = new Random();
+            do
+            {
+                int position = random.Next(0, array.Length);
+                if (array[position] == number)
+                {
+                    return position;
+                }
+                else if (!guessed.Contains(position))
+                {
+                    guessed.Add(position);
+                }
+            }
+            while (guessed.Count < array.Length);
+            return -1;
+        }
+        public int Binary(int[] array, int number) {
+            int low = 0;
+            int high = array.Length - 1;
+            while(low <= high)
+            {
+                int mid = (low + high)  / 2;
+                if (array[mid] == number) { return mid; }
+                else if (array[mid] > number)
+                {
+                    high = mid - 1;
+                }
+                else if (array[mid] < number)
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        
+        
+        }
+
+
+        public int Smort(int[] array, int number) {
+
+            int mid = Math.Abs(array.Length / 2);
+            int[] half;
+            while (array[mid] != number)
+            {
+                if (array[mid] < number)
+                {
+                    half = new int[mid];
+                    for (int i = mid + 1; i < half.Length; i++)
+                    {
+                        half.Append(array[i]);
+                    }
+                    mid = half.Length / 2;
+                }
+                else if (array[mid] > number)
+                {
+                    half = new int[array.Length / 2];
+                    for (int i = mid - 1; i > 0; i--)
+                    {
+                        half.Append(array[i]);
+
+                    }
+                    mid = half.Length / 2;
+                }
+                else if (array[mid] == number)
+                {
+                    return mid;
+                }
+              
+            }
+            return -1;
+        }
+    }
+}
---
+++ programming_advanced/week1/sorting/sorting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =Array.ConvertAll(File.ReadAllLines("sorted_numbers.txt"),Int32.Parse);
+            
+            Guessing guessing = new Guessing();
+            int number = 36;
+            
+            Console.WriteLine($"LINEAR {number} position {guessing.Linear(array, number)}");
+            Console.WriteLine($"STUPID {number} position {guessing.Stupid(array, number)}");
+            Console.WriteLine($"BINARY {number} position {guessing.Binary(array, number)}");
+
+
+            number = 37;
+            Console.WriteLine($"LINEAR {number} position {guessing.Linear(array, number)}");
+            Console.WriteLine($"STUPID {number} position {guessing.Stupid(array, number)}");
+            Console.WriteLine($"BINARY {number} position {guessing.Binary(array, number)}");
+
+
+            number = 998;
+            Console.WriteLine($"LINEAR {number} position {guessing.Linear(array, number)}");
+            Console.WriteLine($"STUPID {number} position {guessing.Stupid(array, number)}");
+            Console.WriteLine($"BINARY {number} position {guessing.Binary(array, number)}");
+
+        }
+    }
+}
