57859dc509ac8a2ff12d54319012fa12024b5bd4
Jente Wauman <72928027+JenteW@users.noreply.github.com>
2023-10-06 15:26:46 +0200

    les03

---
+++ programming_advanced/week03/linkedList/linkedList/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace linkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            Console.WriteLine(list);
+
+            list.Insert(new Node("appel"));
+            list.Insert(new Node("druif"));
+            list.Insert(new Node("banaan"));
+            Console.WriteLine(list);
+
+            list.Insert(new Node("peer"));
+            Console.WriteLine(list);
+            Console.WriteLine(list.Search("peer"));
+
+            Console.WriteLine(list.Search("beans"));
+
+            list.Remove("peer");
+            Console.WriteLine(list);
+            list.Remove("appel");
+            Console.WriteLine(list);
+            list.Remove("banaan");
+            Console.WriteLine(list);
+            list.Remove("druif");
+            Console.WriteLine(list);
+
+            /*****************************STACK******************************/
+
+
+            try
+            {
+                Stack snoep = new Stack(3);
+                Console.WriteLine(snoep);
+                snoep.Push("napoleons");
+                snoep.Push("schepsnoep");
+                snoep.Push("chocolade");
+                Console.WriteLine(snoep);
+                snoep.Push("hartjes");
+
+                Console.WriteLine(snoep);
+                Console.WriteLine(snoep.Pop());
+                Console.WriteLine(snoep);
+                Console.WriteLine(snoep.Pop());
+                Console.WriteLine(snoep);
+                Console.WriteLine(snoep.Pop());
+                Console.WriteLine(snoep);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ programming_advanced/week03/linkedList/linkedList/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedList
+{
+    class Stack
+    {
+        private string[] stack;
+
+        private int top;
+        public Stack(int size)
+        {
+            stack = new string[size];
+            top = 0;
+        }
+        public void Push(string data)
+        {
+            if(top == stack.Length)
+            {
+                throw new Exception("Stack Overflow");
+            }
+            stack[top] = data;
+            top++;
+
+
+
+        }
+        public string Pop()
+        {
+            if(top == 0) throw new Exception("Stack Underflow");
+
+            string temp = stack[top];
+            stack[top-1] = "";
+            top--;
+            return temp;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Join(" | ", stack);
+        }
+    }
+}
