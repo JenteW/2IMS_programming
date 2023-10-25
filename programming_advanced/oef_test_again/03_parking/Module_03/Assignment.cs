53b809e69153722adc062364eb9fc857fe55dc53
Jente Wauman <jente.wauman@gmail.com>
2023-10-25 17:31:42 +0200

    parking

---
+++ programming_advanced/oef_test_again/03_parking/Module_03/Assignment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Module_03
+{
+    internal class Assignment
+    {
+        public string Parking(string input)
+        {
+            try
+            {
+                int parkingSpots = int.Parse(input);
+                List<(int, int)> occupied = new List<(int, int)>();
+                int left = 0;
+
+                for (int i = 0; i < parkingSpots; i++)
+                {
+                    occupied.Add((0, 0)); // Initialize parking spots as unoccupied
+                }
+                string time = Console.ReadLine();
+                string[] hours = time.Split(' ');
+
+                for (int i = 0; i < hours.Length; i++)
+                {
+                    string[] hour = hours[i].Split(':');
+                    int arrive = int.Parse(hour[0]);
+                    int duration = int.Parse(hour[1]);
+
+                    // Check for available parking spot
+                    bool parked = false;
+                    for (int j = 0; j < parkingSpots; j++)
+                    {
+                        if (occupied[j].Item1 <= arrive)
+                        {
+                            occupied[j] = (arrive + duration, i);
+                            parked = true;
+                            break;
+                        }
+                    }
+
+                    // If no parking spot was found, the car had to leave
+                    if (!parked)
+                    {
+                        left++;
+                    }
+                }
+
+                return left.ToString();
+            }
+            catch
+            {
+                return "Crazy input!";
+            }
+        }
+    }
+}
---
+++ programming_advanced/oef_test_again/03_parking/Module_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Module_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Assignment assignment = new Assignment();
+            string input = Console.ReadLine();
+            string output = assignment.Parking(input);
+            Console.WriteLine(output);
+           
+        }
+    }
+}
