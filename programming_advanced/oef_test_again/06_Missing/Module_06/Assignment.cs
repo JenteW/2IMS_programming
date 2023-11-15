71a85a923843e915c1ed1c4aafd83979b7545adc
Jente Wauman <jente.wauman@gmail.com>
2023-11-15 17:22:04 +0100

    big pp

---
+++ programming_advanced/oef_test_again/06_Missing/Module_06/Assignment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module_06
+{
+    internal class Assignment
+    {
+        public List<int> Solve(List<int> list)
+        {
+
+            if (list.Count <= 1)
+            {
+                return list;
+            }
+            int pivot = list[0];
+            List<int> lower = new List<int>();
+            List<int> higher = new List<int>();
+
+            foreach (var item in list)
+            {
+                if (item < pivot)
+                {
+                    lower.Add(item);
+                }
+                else if (item > pivot)
+                {
+                    higher.Add(item);
+                }
+            }
+            return Solve(lower).Union(new List<int>() { pivot }).Union(Solve(higher)).ToList();
+        }
+
+        public int MissingLink(List<int> list)
+        {
+            List<int> sortedList = Solve(list);
+            int number = sortedList[0];
+            int result = -1;
+            bool pos = false;
+            foreach (var item in sortedList)
+            {
+                if (item > 0)
+                {
+                    pos = true;
+                }
+                if (pos)
+                {
+                    number = item;
+                    break;
+                }
+            }
+            foreach (var item in sortedList)
+            {
+                if (item > 0)
+                {
+                    if (item != number && number > 0)
+                    {
+                    result = number;
+                    break;
+                    }
+                
+                    number++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ programming_advanced/oef_test_again/06_Missing/Module_06/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Module_06
+{
+
+    /*Given an unsorted integer array, find the smallest (positive) number in that array
+
+CodeGrade Setup = .Net framework 6.0
+
+ 
+
+Input/Output
+input:
+1 2 3 -1 9 5 7 8
+output:
+4
+
+input:
+-2 -1 2 0 1 3 4
+output:
+No missing link
+
+ 
+
+Code instructions
+
+To solve this assignment you need to create a Module_06 namespace with a class and a method*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assignment assignment = new Assignment();
+                string input = Console.ReadLine();
+                string[] inputArray = input.Split(' ');
+                List<int> list = new List<int>();
+                foreach (var item in inputArray)
+                {
+                    list.Add(int.Parse(item));
+                }
+                int result = assignment.MissingLink(list);
+                if (result == -1)
+                {
+                    Console.WriteLine("No missing link");
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
