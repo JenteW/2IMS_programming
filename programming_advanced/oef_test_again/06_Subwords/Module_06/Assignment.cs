4552a9e46f4fa78fa45fe60b20850680cea4472e
Jente Wauman <jente.wauman@gmail.com>
2023-11-17 21:01:37 +0100

    yes?

---
+++ programming_advanced/oef_test_again/06_Subwords/Module_06/Assignment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Module_06
+{
+    internal class Assignment
+    {
+        public List<string> list = new List<string>();
+        public List<string> solutions = new List<string>();
+
+        public static string[] words()
+        {
+            return File.ReadAllLines("english.txt");
+        }
+        public static bool inFile(string[] words, string input)
+        {
+            return words.Contains(input);
+        }
+        public List<string> GetWords(string input)
+        {
+            List<string> subwords = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 2; j <= input.Length; j++)
+                {
+                    string subword = input.Substring(i, j - i);
+                    if (subword != input) 
+                    {
+                        subwords.Add(subword);
+                    }
+
+                }
+            }
+
+            return subwords.Distinct().OrderBy(s => s).ToList();
+        }
+
+        public List<string> IsValid(List<string> subwords)
+        {
+            List<string> validWords = new List<string>();
+            string[] check = words();
+            foreach (string subword in subwords)
+            {
+                if (inFile(check, subword))
+                {
+                    validWords.Add(subword);
+                }
+            }
+
+            return validWords;
+        }
+       
+    }
+}
--- programming_advanced/oef_test_again/06_Subwords/Module_06/Program.cs
+++ programming_advanced/oef_test_again/06_Subwords/Module_06/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Module_06
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Module_06
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Assignment assignment = new Assignment();
+
+                string input = Console.ReadLine().ToLower();
+                if(input.Length < 2)
+                {
+                    throw new Exception();
+                }
+                List<string> subwords = assignment.GetWords(input);
+                List<string> validWords = assignment.IsValid(subwords);
+
+                Console.WriteLine(string.Join(" ", validWords));
+            }
+            catch
+            {
+                Console.WriteLine("Crazy input!");
+            }
         }
     }
 }
