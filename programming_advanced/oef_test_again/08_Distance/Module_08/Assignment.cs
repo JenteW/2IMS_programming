43fac841f4f80a977b143e536a3e2f9c6c603126
Jente Wauman <jente.wauman@gmail.com>
2023-12-01 17:22:17 +0100

    stonks

---
+++ programming_advanced/oef_test_again/08_Distance/Module_08/Assignment.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module_08
+{
+    internal class Assignment
+    {
+        private int nodes;
+        List<int>[] graph;
+        List<int> visited = new List<int>();
+
+        public Assignment(int nodes)
+        {
+            this.nodes = nodes;
+            graph = new List<int>[nodes];
+            for (int i = 0; i < nodes; i++)
+            {
+                graph[i] = new List<int>();
+            }
+            visited = new List<int>();
+        }
+
+        public void AddEdge(int node1, int node2)
+        {
+            graph[node1].Add(node2);
+            graph[node2].Add(node1);
+
+        }
+
+        public int[] BFS(int startnode)
+        {
+            Queue<int> queue = new Queue<int>();
+            List<int> visited = new List<int>();
+            int[] distance = new int[nodes];
+            for(int i = 0; i < nodes; i++)
+            {
+                distance[i] = -1;
+            }
+
+
+            queue.Enqueue(startnode);
+            distance[startnode] = 0;
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+
+               /* if (!visited.Contains(node))
+                {
+                    visited.Add(node);
+                }
+
+                if (node == 0)
+                {
+                    return distance;
+
+                }*/
+
+                foreach (var nextNode in graph[node])
+                {
+                    if (distance[nextNode] == -1)
+                    {
+                        distance[nextNode] = distance[node] + 1;
+                        queue.Enqueue(nextNode);
+                    }
+                    /*if (!visited.Contains(nextNode))
+                    {
+                        queue.Enqueue(nextNode);
+                    }*/
+                }
+
+            }
+            return distance;
+
+        }
+    }
+}
---
+++ programming_advanced/oef_test_again/08_Distance/Module_08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            int nodes = Int32.Parse(Console.ReadLine()); 
+            int edges = Int32.Parse(Console.ReadLine());
+            Assignment assignment = new Assignment(nodes);
+            for (int i = 0; i < edges; i++)
+            {
+                string[] edge = Console.ReadLine().Split(' ');
+                assignment.AddEdge(Int32.Parse(edge[0]), Int32.Parse(edge[1]));
+            }
+            int startnode = Int32.Parse(Console.ReadLine());
+            int[] distance = assignment.BFS(startnode);
+            for (int i = 0; i < distance.Length; i++)
+            {
+                Console.WriteLine(distance[i]);
+            }
+            }
+            catch 
+            {
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
